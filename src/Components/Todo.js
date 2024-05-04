77f084ba85ef371a64e63f2a8e31b3fde7151440
Hassam Amer <hasideveloper@gmail.com>
2024-05-04 14:56:24 +0500

    working in useSelector

--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -1,11 +1,18 @@
 
 
+import {useSelector, useDispatch} from 'react-redux'
+import {removeTodo} from '../Features/todo/todoSlice'
+
+
 const Todo = () => {
+const todo=useSelector(state=>state.todo)
+const dispatch=useDispatch()
+
   return (
     <div>
-      
+      Todos
     </div>
   )
-}
+} 
 
 export default Todo 
